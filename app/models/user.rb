a438580ad37cf00cfbe01f1237cbc82125cd99d8
reverson1 <richardeverson@hotmail.com>
2014-04-11 11:10:27 -0400

    added tests for user registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,19 +11,20 @@ class User < ActiveRecord::Base
         :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  # attr_accessible :email, :password, :password_confirmation, :remember_me, 
-  # :first_name, :last_name, :profile_name  
+  attr_accessible :email, :password, :password_confirmation, :remember_me, 
+                  :first_name, :last_name, :profile_name  
+
   has_many :statuses
 
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :profile_name, presence: true,
-                           uniqueness: true
-
-
-  def self.authenticate_safely(user_id, password)
-    where("user_id= ? AND password = ?", user_id, password).first
-  end
+  
+  validates :email, presence: true, uniqueness: true
+  
+  validates :profile_name, presence: true, uniqueness: true, format: {
+                            with: /\A[a-zA-Z\-\_]+\Z/,
+                            message: 'Must be formatted correctly'
+                           }      
 
   def fullname
     first_name + ' ' + last_name
