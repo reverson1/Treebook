67143a041e82960eeb6684f9ae87870c7865e5fa
reverson1 <richardeverson@hotmail.com>
2014-04-12 14:15:46 -0400

    wrote status tests

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,4 +1,9 @@
 class Status < ActiveRecord::Base
 	belongs_to :user 
 	attr_accessible :content, :user_id
+
+	validates :content, presence: true,
+						length: {minimum: 2}
+
+	validates :user_id, presence: true
 end
